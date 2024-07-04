4602878dfc17f7fa03d28f1cbfc698cc095066ec
InSung0522 <pkani1027@gmail.com>
2024-07-04 18:04:00 +0900

    Learned Collection with vs2019 on labtop

---
+++ Study_Cs_05/Study_Cs_05/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Cs_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열리스트. 자료형이 달라도 되는 리스트
+            Console.WriteLine("배열 리스트");
+            ArrayList al = new ArrayList();
+
+            al.Add(1);
+            al.Add("Hello");
+            al.Add(3.3);
+            al.Add(true);
+
+            foreach(var item in al)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            al.Remove("Hello");
+
+            foreach(var item in al)
+            {
+                Console.WriteLine(item);
+            }
+
+            //큐 FIFO
+            Console.WriteLine("\n큐");
+            Queue qu = new Queue();
+
+            qu.Enqueue(1);
+            qu.Enqueue(2);
+            qu.Enqueue(3);
+
+            while (qu.Count > 0)
+            {
+                Console.WriteLine(qu.Dequeue());
+            }
+
+            //스택 LIFO
+            Console.WriteLine("\n스택");
+            Stack st = new Stack();
+
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+
+            while (st.Count > 0)
+            {
+                Console.WriteLine(st.Pop());
+            }
+
+            //해시테이블 Key-Value
+            Console.WriteLine("\n해시테이블");
+        }
+    }
+}
