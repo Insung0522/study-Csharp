56458d4409b43ca1dfdc7af80562588e2a193086
InSung0522 <pkani1027@gmail.com>
2024-07-05 09:52:41 +0900

    vs2019 환경에서 winForm을 활용하여 로그인 창을 만듦

---
+++ Study_Cs_07_WinForm/Study_Cs_07_WinForm/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Study_Cs_07_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // login_button
+            // 
+            this.login_button.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.login_button.Location = new System.Drawing.Point(180, 329);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(301, 57);
+            this.login_button.TabIndex = 0;
+            this.login_button.Text = "로그인";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.UseWaitCursor = true;
+            this.login_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(249, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "로그인 창";
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.id.Location = new System.Drawing.Point(140, 158);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(412, 46);
+            this.id.TabIndex = 2;
+            this.id.Text = "아이디";
+            this.id.UseWaitCursor = true;
+            this.id.TextChanged += new System.EventHandler(this.id_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.password.Location = new System.Drawing.Point(140, 244);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(412, 46);
+            this.password.TabIndex = 4;
+            this.password.Text = "패스워드";
+            this.password.UseSystemPasswordChar = true;
+            this.password.UseWaitCursor = true;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 524);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox password;
+    }
+}
+
