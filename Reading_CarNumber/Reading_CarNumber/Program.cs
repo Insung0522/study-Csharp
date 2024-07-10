8a4a1e8cd52a4e92424f921803bc83caf4d7fead
ispark96 <pkani1027@gmail.com>
2024-07-10 17:59:16 +0900

    차량번호 인식 중간 저장

--- Reading_CarNumber/Reading_CarNumber/Program.cs
+++ Reading_CarNumber/Reading_CarNumber/Program.cs
@@ -26,9 +26,10 @@ namespace Reading_CarNumber
             //Cv2.DestroyAllWindows();
 
             //Mat image = Cv2.ImRead("../../../../carnumber_a01.png", ImreadModes.Grayscale);
-            Mat image = new Mat("../../../../carnumber_a01.png"); //원문
-            //Mat image = new Mat("../../../../carnumber.jpg");
+            //Mat image = new Mat("../../../../carnumber_a01.png"); //원문
+            Mat image = new Mat("../../../../carnumber.jpg");
             //Cv2.ImShow("image", image);
+            Mat tmp = new Mat();
 
             //그레이스케일
             Mat gray = new Mat();
@@ -38,18 +39,17 @@ namespace Reading_CarNumber
             //가우시안 블러
             Mat gaussianblur = new Mat();
             Cv2.GaussianBlur(gray, gaussianblur, new Size(5, 5), 0);
-            Cv2.ImShow("gaussianBlur", gaussianblur);
+            //Cv2.ImShow("gaussianBlur", gaussianblur);
 
             //캐니
             Mat canny = new Mat();
             Cv2.Canny(gaussianblur, canny, 100, 300, 3);
-            Cv2.ImShow("canny",canny);
+            Cv2.ImShow("canny", canny);
 
             //이진화 (Thresholding)
             Mat binary = new Mat(); //이진화될 공간
             Cv2.Threshold(canny, binary, 157, 255, ThresholdTypes.Binary);
             Cv2.ImShow("binary", binary);
-
 
             //윤곽선 검출
             Mat black = new Mat();
@@ -60,8 +60,6 @@ namespace Reading_CarNumber
 
             Cv2.InRange(binary, new Scalar(255, 255, 255), new Scalar(255, 255, 255), black);
             Cv2.FindContours(black, out contours, out hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxTC89KCOS);
-            Cv2.ImShow("contours", contours);
-
 
             //불필요한 윤곽선 제거
             List<Point[]> new_contours = new List<Point[]>();
@@ -73,27 +71,32 @@ namespace Reading_CarNumber
                 //{
                 //new_contours.Add(p);
                 //}
-                Console.WriteLine("length : " + length); //100 ~ 200
-                Console.WriteLine("area : " + area); // 200 ~ 700
-                if ((length < 100 && area < 200) || (length > 200 && area > 700) && p.Length > 5) continue;
+                //Console.WriteLine("length : " + length); //100 ~ 200
+                //Console.WriteLine("area : " + area); // 200 ~ 700
+                //if ((length < 100 && area < 200) || (length > 200 && area > 700) && p.Length > 5) continue;
 
                 Rect boundingRect = Cv2.BoundingRect(p);
+                Console.WriteLine("Height : " + boundingRect.Height);
+                Console.WriteLine("Width : " + boundingRect.Width);
+
+                double ratio = boundingRect.Width / boundingRect.Height;
+                Console.WriteLine("Ratio : " + ratio);
+
+                if (ratio != 1) continue;
                 new_contours.Add(p);
-                Cv2.Rectangle(drawing, boundingRect, Scalar.Red, 2);
-                //Cv2.ImShow("dst", dst);
+                Cv2.Rectangle(drawing, boundingRect, Scalar.White, 2);
+                Cv2.ImShow("drawing", drawing);
                 //Cv2.WaitKey(0);
 
             }
-
-            Cv2.DrawContours(drawing, new_contours, -1, new Scalar(255, 0, 0), 2, LineTypes.AntiAlias, null, 1);
-            Cv2.ImShow("drawing", drawing);
-
-            //데이터 비교
             
+            //Cv2.DrawContours(drawing, new_contours, -1, new Scalar(255, 0, 0), 2, LineTypes.AntiAlias, null, 1);
 
-
+            Cv2.ImShow("drawing", drawing);
             Cv2.WaitKey(0);
             Cv2.DestroyAllWindows();
+
+
         }
     }
 }
